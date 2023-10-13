9b2ac8d116ec51f5cb99622fac034112e8e0a0a3
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2023-10-13 12:45:24 -0400

    readme

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Liya Getachew
+ * Created on: Oct 2023
+ * This program creates traffic lights with Neopixels
 */
 
 basic.showString('Hello, World!')
