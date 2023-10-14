f6e8bbaa09ab65d4ae4906a2c24528634ca4ab6b
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2023-10-14 16:39:50 +0000

    py vers

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,11 +1,34 @@
 """
 Created by: Liya Getachew
 Created on: Oct 2023
-This module is a Micro:bit MicroPython program that creates a traffic light simulation with Neopixels.
+This module is a Micro:bit MicroPython program that creates a traffic light with Neopixels.
 """
 
 from microbit import *
-from random import *
+import neopixel
 
+display.clear()
+display.show(Image.HEART)
 
+neopixelStrip = neopixel.NeoPixel(pin16, 4)
 
+while True:
+    if button_a.is_pressed():
+        neopixelStrip[0] = (0, 225, 0)
+        print(neopixelStrip[0])
+        neopixelStrip.show()
+        sleep(2000)
+        neopixelStrip[0] = (0, 0, 0)
+        neopixelStrip[1] = (225, 225, 0)
+        neopixelStrip.show()
+        print(neopixelStrip[1])
+        sleep(2000)
+        neopixelStrip[1] = (0, 0, 0)
+        neopixelStrip[2] = (225, 0, 0)
+        neopixelStrip.show()
+        print(neopixelStrip[2])
+        sleep(2000)
+        neopixelStrip[2] = (0, 0, 0)
+        print(neopixelStrip[2])
+        neopixelStrip.show()
+        display.show(Image.HEART_SMALL)
